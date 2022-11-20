375a531c37455bffdcf9c3fd345e53003c4624ac
unknown <xx06102002@gmail.com>
2022-11-20 16:24:27 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Escribir un programa que lea números enteros indefinidamente hasta que llegue el número 0
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 1;
+
+        while(num != 0)
+        {
+        Console.WriteLine("Digite un número");
+        num = int.Parse(Console.ReadLine());
+ 
+        }
+        Console.WriteLine("Finalizado");
+    }
+}
